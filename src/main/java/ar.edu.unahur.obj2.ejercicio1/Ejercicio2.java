e6e191269de574ede87106c814872a532fac4522
santismith <santii.smiith@hotmail.com>
2020-04-09 16:51:36 -0300

    ej2

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio2.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio2.java
@@ -1,5 +1,24 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
-    
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in) ;
+        int num = 3 ;
+
+        int v[] = new int[5] ;
+
+        for (int i = 0 ;i<= v.length-1 ; i++){
+            System.out.println("Ingrese numero ");
+            v[i]= teclado.nextInt();
+        }
+
+        for (int i=0 ; i<=v.length-1 ; i++) {
+            if (v[i]>num) {
+                System.out.println("Los numeros mayores a " + num + "son:" + v[i] + " ");
+            }
+        }
+    }
 }
