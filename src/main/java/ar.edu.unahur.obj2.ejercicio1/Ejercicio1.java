c15cdd58b596533fe994dd20fdf9efcbe107e854
santismith <santii.smiith@hotmail.com>
2020-04-09 16:27:31 -0300

    ej1

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
@@ -1,8 +1,17 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
-    public int[] ordenar() {
-        return new int[] {};
+    public static void main(String[] args) {
+
+        //Scanner reader = new Scanner(System.in) ;
+
+        int v[] = new int[] {1,2,3,4,5,6,7,8,9,10} ;
+
+        for(int i = v.length-1 ; i >=0 ; i-- ) {
+            System.out.print(v[i]+ " ");
+        }
     }
 }
---
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio2.java
@@ -0,0 +1,5 @@
+package ar.edu.unahur.obj2.ejercicio1;
+
+public class Ejercicio2 {
+    
+}
